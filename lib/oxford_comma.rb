3c950ada117f2b80a2d194cdf109624524eb94e7
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-01 21:58:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
-array = 
-def oxford_comma(array)
-
+def oxford_comma(words)
+  if words.length == 2
+    return "#{words[0]} and #{words[1]}"
+  elsif words.length > 2
+    words[-1].insert(0 , "and ")
+  end
+  array.join(", ")    
 end
