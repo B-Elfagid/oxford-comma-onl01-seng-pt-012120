4a3b58c30a9d07adb4b76861be1fd38c450aa741
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-01 22:11:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@
 array = ["cat"]
 def oxford_comma(array)
   if array.length == 2
-  array[-2] << " and " array.join
+  array[-2] << " and " 
+  array.join
   elsif array.length == 1
   	array.join
   elsif array.length > 2
